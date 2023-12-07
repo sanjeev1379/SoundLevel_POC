b03cd5be9923dbde29d2be5de935d188f59efc6d
Ramaguru Siva <ramaguru.siva@aspiresys.com>
2023-12-07 10:51:55 +0530

    feat: fix babel issue

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-react',
-        '@babel/preset-flow',
-        'module:metro-react-native-babel-preset'
-    ],
-    plugins: [
-        'babel-plugin-styled-components',
-        '@babel/plugin-proposal-class-properties'
-    ]
+    presets: ['module:metro-react-native-babel-preset']
 };
